561c923aabfd2cee9f99c4bad2e7d60cee2d518c
Sean <38871906+seancrain@users.noreply.github.com>
2026-02-14 17:04:22 -0600

    Make road textures connect

--- src/main/java/com/nosiphus/nosiphuscustomblocks/world/level/block/RoadBlock.java
+++ src/main/java/com/nosiphus/nosiphuscustomblocks/world/level/block/RoadBlock.java
@@ -2,6 +2,7 @@ package com.nosiphus.nosiphuscustomblocks.world.level.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.util.StringRepresentable;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
@@ -10,21 +11,40 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
-import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import org.jetbrains.annotations.Nullable;
 
 public class RoadBlock extends Block {
 
     public static final EnumProperty<Direction.Axis> AXIS = BlockStateProperties.HORIZONTAL_AXIS;
-    public static final IntegerProperty WIDTH = IntegerProperty.create("width", 1, 7);
-    public static final IntegerProperty POSITION = IntegerProperty.create("position", 0, 6);
+    public static final EnumProperty<RoadTexture> TEXTURE = EnumProperty.create("texture", RoadTexture.class);
 
     public RoadBlock(Properties properties) {
         super(properties);
         this.registerDefaultState(this.stateDefinition.any()
                 .setValue(AXIS, Direction.Axis.X)
-                .setValue(WIDTH, 1)
-                .setValue(POSITION, 0));
+                .setValue(TEXTURE, RoadTexture.LANE));
+    }
+
+    public enum RoadTexture implements StringRepresentable {
+        DIVIDER("divider"),
+        DIVIDER_LEFT("divider_left"),
+        DIVIDER_RIGHT("divider_right"),
+        LANE("lane"),
+        SINGLE("single"),
+        SHOULDER_LEFT("shoulder_left"),
+        SHOULDER_LEFT_DIVIDER("shoulder_left_divider"),
+        SHOULDER_RIGHT("shoulder_right"),
+        SHOULDER_RIGHT_DIVIDER("shoulder_right_divider");
+
+        private final String name;
+        RoadTexture(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String getSerializedName() {
+            return this.name;
+        }
     }
 
     @Override
@@ -42,30 +62,46 @@ public class RoadBlock extends Block {
 
     private BlockState calculateState(Level world, BlockPos pos, BlockState state) {
         Direction.Axis axis = state.getValue(AXIS);
-        // Determine the "Scanning" direction perpendicular to the road's flow
         Direction checkDir = (axis == Direction.Axis.X) ? Direction.SOUTH : Direction.EAST;
         Direction oppDir = checkDir.getOpposite();
 
         int left = 0;
         int right = 0;
 
-        // Scan "Left"
         while (left < 6 && world.getBlockState(pos.relative(checkDir, left + 1)).getBlock() instanceof RoadBlock) {
             left++;
         }
-        // Scan "Right"
         while (right < 6 && world.getBlockState(pos.relative(oppDir, right + 1)).getBlock() instanceof RoadBlock) {
             right++;
         }
 
-        int totalWidth = Math.min(7, left + right + 1);
-        int myPos = Math.min(6, left);
+        int totalWidth = left + right + 1;
+        int myPos = left;
+        RoadTexture tex = RoadTexture.LANE;
 
-        return state.setValue(WIDTH, totalWidth).setValue(POSITION, myPos);
+        if (totalWidth == 1) {
+            tex = RoadTexture.SINGLE;
+        } else if (totalWidth == 2) {
+            tex = (myPos == 0) ? RoadTexture.SHOULDER_LEFT_DIVIDER : RoadTexture.SHOULDER_RIGHT_DIVIDER;
+        } else {
+            if (myPos == 0) {
+                tex = RoadTexture.SHOULDER_LEFT;
+            } else if (myPos == totalWidth - 1) {
+                tex = RoadTexture.SHOULDER_RIGHT;
+            } else if (totalWidth % 2 != 0 && myPos == totalWidth / 2) {
+                tex = RoadTexture.DIVIDER;
+            } else if (totalWidth % 2 == 0 && myPos == (totalWidth / 2) - 1) {
+                tex = RoadTexture.DIVIDER_LEFT;
+            } else if (totalWidth % 2 == 0 && myPos == totalWidth / 2) {
+                tex = RoadTexture.DIVIDER_RIGHT;
+            }
+        }
+
+        return state.setValue(TEXTURE, tex);
     }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(AXIS, WIDTH, POSITION);
+        builder.add(AXIS, TEXTURE);
     }
 }
