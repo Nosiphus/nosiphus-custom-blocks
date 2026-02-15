720129c0c0906b970dae682e10371d85b12d34bf
Sean <38871906+seancrain@users.noreply.github.com>
2026-02-14 23:08:24 -0600

    Add 1x1 t variants

--- src/main/java/com/nosiphus/nosiphuscustomblocks/world/level/block/RoadBlock.java
+++ src/main/java/com/nosiphus/nosiphuscustomblocks/world/level/block/RoadBlock.java
@@ -39,7 +39,11 @@ public class RoadBlock extends Block {
         SHOULDER_DIVIDER_RIGHT("shoulder_divider_right"),
         SHOULDER_LEFT("shoulder_left"),
         SHOULDER_RIGHT("shoulder_right"),
-        SHOULDER_SINGLE("shoulder_single");
+        SHOULDER_SINGLE("shoulder_single"),
+        SHOULDER_SINGLE_T_NORTH("shoulder_single_t_north"),
+        SHOULDER_SINGLE_T_SOUTH("shoulder_single_t_south"),
+        SHOULDER_SINGLE_T_EAST("shoulder_single_t_east"),
+        SHOULDER_SINGLE_T_WEST("shoulder_single_t_west");
 
         private final String name;
         RoadTexture(String name) {
@@ -68,7 +72,21 @@ public class RoadBlock extends Block {
     private BlockState calculateState(Level world, BlockPos pos, BlockState state) {
         Direction.Axis axis = state.getValue(AXIS);
 
-        if (axis == Direction.Axis.Y) return state.setValue(TEXTURE, RoadTexture.SHOULDER_SINGLE);
+        if (axis == Direction.Axis.Y) {
+            boolean n = world.getBlockState(pos.north()).getBlock() instanceof RoadBlock;
+            boolean s = world.getBlockState(pos.south()).getBlock() instanceof RoadBlock;
+            boolean e = world.getBlockState(pos.east()).getBlock() instanceof RoadBlock;
+            boolean w = world.getBlockState(pos.west()).getBlock() instanceof RoadBlock;
+            int neighbors = (n?1:0) + (s?1:0) + (e?1:0) + (w?1:0);
+
+            if (neighbors == 3) {
+                if (!n) return state.setValue(TEXTURE, RoadTexture.SHOULDER_SINGLE_T_NORTH);
+                if (!s) return state.setValue(TEXTURE, RoadTexture.SHOULDER_SINGLE_T_SOUTH);
+                if (!e) return state.setValue(TEXTURE, RoadTexture.SHOULDER_SINGLE_T_EAST);
+                if (!w) return state.setValue(TEXTURE, RoadTexture.SHOULDER_SINGLE_T_WEST);
+            }
+            return state.setValue(TEXTURE, RoadTexture.SHOULDER_SINGLE);
+        }
 
         Direction widthDir = (axis == Direction.Axis.X) ? Direction.SOUTH : Direction.EAST;
         Direction flowDir = (axis == Direction.Axis.X) ? Direction.EAST : Direction.SOUTH;
@@ -76,38 +94,31 @@ public class RoadBlock extends Block {
         int left = 0, right = 0;
         while (left < 6 && isRoadAxis(world, pos.relative(widthDir, left + 1), axis)) left++;
         while (right < 6 && isRoadAxis(world, pos.relative(widthDir.getOpposite(), right + 1), axis)) right++;
-
         int width = left + right + 1, myPos = left;
 
         BlockState f = world.getBlockState(pos.relative(flowDir)), b = world.getBlockState(pos.relative(flowDir.getOpposite()));
-        boolean nearInt = (f.is(this) && f.getValue(AXIS) == Direction.Axis.Y) || (b.is(this) && b.getValue(AXIS) == Direction.Axis.Y);
-
-        if (nearInt) {
+        if ((f.is(this) && f.getValue(AXIS) == Direction.Axis.Y) || (b.is(this) && b.getValue(AXIS) == Direction.Axis.Y)) {
             if (width == 1) return state.setValue(TEXTURE, RoadTexture.CROSSWALK_SINGLE);
             if (myPos == 0) return state.setValue(TEXTURE, RoadTexture.SHOULDER_CROSSWALK_LEFT);
             if (myPos == width - 1) return state.setValue(TEXTURE, RoadTexture.SHOULDER_CROSSWALK_RIGHT);
             return state.setValue(TEXTURE, RoadTexture.CROSSWALK);
         }
 
-        return state.setValue(TEXTURE, getRoadTexture(width, myPos));
+        if (width == 1) return state.setValue(TEXTURE, RoadTexture.SINGLE);
+        if (width == 2) return state.setValue(TEXTURE, (myPos == 0) ? RoadTexture.SHOULDER_DIVIDER_LEFT : RoadTexture.SHOULDER_DIVIDER_RIGHT);
+        if (myPos == 0) return state.setValue(TEXTURE, RoadTexture.SHOULDER_LEFT);
+        if (myPos == width - 1) return state.setValue(TEXTURE, RoadTexture.SHOULDER_RIGHT);
+        if (width % 2 != 0 && myPos == width / 2) return state.setValue(TEXTURE, RoadTexture.ODD_DIVIDER);
+        if (width % 2 == 0) {
+            if (myPos == (width / 2) - 1) return state.setValue(TEXTURE, RoadTexture.EVEN_DIVIDER_LEFT);
+            if (myPos == width / 2) return state.setValue(TEXTURE, RoadTexture.EVEN_DIVIDER_RIGHT);
+        }
+        return state.setValue(TEXTURE, RoadTexture.LANE);
     }
 
     private boolean isRoadAxis(Level world, BlockPos pos, Direction.Axis axis) {
         BlockState state = world.getBlockState(pos);
         return state.is(this) && state.getValue(AXIS) == axis;
-    }
-
-    private RoadTexture getRoadTexture(int width, int pos) {
-        if (width == 1) return RoadTexture.SINGLE;
-        if (width == 2) return (pos == 0) ? RoadTexture.SHOULDER_DIVIDER_LEFT : RoadTexture.SHOULDER_DIVIDER_RIGHT;
-        if (pos == 0) return RoadTexture.SHOULDER_LEFT;
-        if (pos == width - 1) return RoadTexture.SHOULDER_RIGHT;
-        if (width % 2 != 0 && pos == width / 2) return RoadTexture.ODD_DIVIDER;
-        if (width % 2 == 0) {
-            if (pos == (width / 2) - 1) return RoadTexture.EVEN_DIVIDER_LEFT;
-            if (pos == width / 2) return RoadTexture.EVEN_DIVIDER_RIGHT;
-        }
-        return RoadTexture.LANE;
     }
 
     @Override
